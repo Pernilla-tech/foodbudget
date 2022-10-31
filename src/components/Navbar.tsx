b783e2ad3a40ac739c515ca6116ca60330944515
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-10-31 20:15:56 +0100

    add log out button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 import { Container, Navbar as NavbarBs, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { auth } from "../firebaseApp";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Navbar = () => {
+	const [user] = useAuthState(auth);
+
+	console.log(user)
+
+	const logout = () => {
+		signOut(auth).then(() => document.location.reload());
+	};
 	return (
 		<NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
 			<Container className="me-auto">
@@ -12,9 +22,25 @@ const Navbar = () => {
 					<Nav.Link to="/history" as={NavLink}>
 						Historik
 					</Nav.Link>
+
 				</Nav>
+				<div style={{ display: "flex" }}>
+					{user ? (
+						<><img style={{
+							borderRadius: "50px",
+							height: "30px"
+						}} src={user.photoURL || ""} />
+							<p style={{ margin: "0", paddingRight: "4px", paddingLeft: "4px" }}> {user.displayName}</p></>
+					) : (
+						null
+					)
+
+					}
+
+					<Button onClick={logout}>Sign out</Button>
+				</div>
 			</Container>
-		</NavbarBs>
+		</NavbarBs >
 	);
 };
 
