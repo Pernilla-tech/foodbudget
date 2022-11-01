b21c2a35ed7cf8f83ff95cf454100c90933ec6e0
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-11-01 14:37:57 +0100

    delete unused file and comments

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// Import the functions you need from the SDKs you need
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBQ3ExaKomZfOFy7ofOlQXt-AZioLkJdp8",
@@ -19,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
+
 
 export default firebaseApp;
