0d9b1f48d1b6cc0ef41a3a5a8006ab90ce33dec3
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-11-01 14:36:27 +0100

    moved code to landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
-import MainView from "./mainView";
-import {
-	browserLocalPersistence,
-	setPersistence,
-} from "firebase/auth";
-import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "./firebaseApp";
-import { Button } from "react-bootstrap";
+
+
+import LandingPage from "./components/LandingPage";
 import "./styles.css";
+
 const App = () => {
-	const [user, loading, error] = useAuthState(auth);
-	const [signInWithGoogle, , gLoading, gError] = useSignInWithGoogle(auth);
 
-	const authError = error || gError;
-	const isLoading = loading || gLoading;
-
-	const login = async () => {
-		await setPersistence(auth, browserLocalPersistence);
-		signInWithGoogle();
-	};
-
-	if (authError) {
-		return (
-			<div>
-				<p style={{ color: "white" }}>Error: {authError.message}</p>
-			</div>
-		);
-	}
-
-	if (isLoading) {
-		return <p style={{ color: "white" }}>Loading...</p>;
-	}
-
-	if (user) {
-		return (
-			<>
-				<MainView />
-			</>
-		);
-	}
 
 	return (
 		<div className="App">
-			<div style={{ display: "flex", alignItems: "center", flexDirection: "column", marginTop: "25%" }}>
-				<h1 style={{ color: "white", padding: "24px" }}>
-					Keep track of your budget
-				</h1>
-				<Button
-					disabled={isLoading}
-					onClick={login}
-					style={{ backgroundColor: "rgb(255, 107, 53)", border: "none" }}
-				>
-					Sign In
-				</Button>
-			</div>
+			<LandingPage />
 		</div>
 	);
 };
